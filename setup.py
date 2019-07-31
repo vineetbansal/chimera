eea0a2015890c09a0cdc51f844200f6f4c032c45
Vineet Bansal <vineetbansal@gmail.com>
2019-07-31 11:11:51 -0400

    App version no. in template footer. Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='chimera',
-    version='0.1.2',
+    version='0.1.3',
     description='Protein Domain Identification Package',
     url='https://github.com/vineetbansal/chimera',
     author='Vineet Bansal',
